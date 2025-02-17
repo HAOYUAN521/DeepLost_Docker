9d52737672bfe09e20a18d689e036aeac960fbf6
Haoyuan Wang <126190279+HAOYUAN521@users.noreply.github.com>
2025-02-17 11:59:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import time
-
 import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
-
+cache = redis.Redis(host='redis', port=6379, decode_responses=True)  # Added decode_responses=True
 
 def get_hit_count():
     retries = 5
@@ -18,8 +16,10 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
-
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return f'Hello World! I have been seen {count} times.\n'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)  # Keeps the server running
